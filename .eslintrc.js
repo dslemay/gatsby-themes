060242b9ff6367bc3169d2fb9a26f01a822bd0d8
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-03-19 21:53:25 -0700

    Set up ESLint and lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+    'jest/globals': true,
+  },
+  settings: {
+    ecmascript: 6,
+    jsx: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+      experimentalDecorators: true,
+      jsx: true,
+    },
+    sourceType: 'module',
+  },
+  plugins: ['react', 'jest', 'prettier'],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'arrow-parens': ['error', 'as-needed'],
+    'jsx-a11y/label-has-for': 'off',
+    'jsx-a11y/label-has-associated-control': 'error',
+  },
+};
