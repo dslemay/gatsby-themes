fcfc705af56ab5d091365b068eef39d1a326bf38
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-03-19 21:53:48 -0700

    Convert to single quotes

--- packages/gatsby-theme-dslemay-core/gatsby-config.js
+++ packages/gatsby-theme-dslemay-core/gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   plugins: [
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    "gatsby-plugin-sitemap"
-  ]
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sitemap',
+  ],
 };
