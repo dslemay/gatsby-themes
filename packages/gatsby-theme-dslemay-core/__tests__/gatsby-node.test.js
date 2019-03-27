7395cb414aa498338ccdd86e7a09b3cfb2d3853c
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-03-27 12:47:46 -0700

    chore: add tests for gatsby-node

---
+++ packages/gatsby-theme-dslemay-core/__tests__/gatsby-node.test.js
@@ -0,0 +1,45 @@
+const mockMkdirp = require('mkdirp');
+const mockFs = require('fs');
+
+const { onPreBootstrap } = require('../gatsby-node');
+
+jest.mock('mkdirp');
+
+jest.mock('path', () => ({
+  join: (...args) => args.join('/'),
+}));
+
+jest.mock('fs', () => ({
+  existsSync: jest.fn(),
+}));
+
+const mockConfig = {
+  reporter: {
+    log: () => {},
+  },
+  store: {
+    getState: () => ({
+      program: {
+        directory: 'test',
+      },
+    }),
+  },
+};
+
+afterEach(mockFs.existsSync.mockReset);
+
+describe('onPreBootstrap', () => {
+  it('does not create directories if the directory already exists', () => {
+    mockFs.existsSync.mockImplementation(() => true);
+
+    onPreBootstrap(mockConfig);
+    expect(mockMkdirp.sync).not.toBeCalled();
+  });
+
+  it('creates directories if the directory does not exist', () => {
+    onPreBootstrap(mockConfig);
+    mockFs.existsSync.mockImplementation(() => false);
+    expect(mockMkdirp.sync).toBeCalled();
+    expect(mockMkdirp.sync).toBeCalledTimes(2);
+  });
+});
