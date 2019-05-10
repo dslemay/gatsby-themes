eab7398cb1c89522eff114d04b53e91b001ce49d
Daniel Lemay <26767995+dslemay@users.noreply.github.com>
2019-05-09 17:12:15 -0700

    chore: configure project to use my eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,3 @@
 module.exports = {
-  parser: 'babel-eslint',
-  env: {
-    browser: true,
-    es6: true,
-    'jest/globals': true,
-  },
-  settings: {
-    ecmascript: 6,
-    jsx: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2017,
-    ecmaFeatures: {
-      experimentalObjectRestSpread: true,
-      experimentalDecorators: true,
-      jsx: true,
-    },
-    sourceType: 'module',
-  },
-  plugins: ['react', 'jest', 'prettier'],
-  extends: [
-    'airbnb',
-    'prettier',
-    'plugin:prettier/recommended',
-    'prettier/react',
-  ],
-  rules: {
-    'react/jsx-filename-extension': 'off',
-    'arrow-parens': ['error', 'as-needed'],
-    'jsx-a11y/label-has-for': 'off',
-    'jsx-a11y/label-has-associated-control': 'error',
-  },
+  extends: ['dslemay', 'dslemay/jest', 'dslemay/react', 'dslemay/jsx-a11y'],
 };
